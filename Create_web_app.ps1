f988570a3890b4f4d3f04059a8c0bc421fc584cc
Koremochi <84866292+Koremochi@users.noreply.github.com>
2024-05-28 08:10:50 +0200

    Add .ps1 file and ARM JSON file
    Exported Resource group template from Azure and powershell script for creating an Azure web app

---
+++ Create_web_app.ps1
@@ -0,0 +1,4 @@
+﻿$name = Read-Host "Name your web app. Don't start the name with a special character"
+
+Connect-AzAccount
+New-AzWebApp -ResourceGroupName "rg-syskitassignment-eunorth" -Name $name -Location "North Europe" -AppServicePlan "ASP-rgsyskitassignmenteunorth-ba58"
